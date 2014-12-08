bc98dfe3a2f852d7aaa656127f13767341d3cbaf
Joshua Cooper <me@ijo.sh>
2014-12-08 08:40:29 +0000

    Added the read, update functions and tests

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 
 var request = require('request'),
-	//events = require('events'),
+    util = require('util'),
+	events = require('events'),
 	//constants = require('./lib/constants'),
 	host = 'tsdb.info',
 	port = 80,
@@ -21,83 +22,107 @@ function TSDBClient(data, options) {
 
 	var self = this;
 
-    this.data.on("connect", function () {
-        self.on_connect();
-    });
-
-    this.data.on("data", function (buffer_from_socket) {
-        self.on_data(buffer_from_socket);
-    });
-
-    this.data.on("error", function (msg) {
-        self.on_error(msg.message);
-    });
-
-    this.data.on("close", function () {
-        self.connection_gone("close");
-    });
-
     events.EventEmitter.call(this);
 
-}
-
-TSDBClient.prototype.on_error = function (msg) {
-    var message = "TSDB connection to " + this.host + ":" + this.port + " failed - " + msg;
-
-    if (this.closing) {
-        return;
-    }
-
-    if (exports.debug_mode) {
-        console.warn(message);
-    }
-
-    this.emit("error", new Error(message));
-    // "error" events get turned into exceptions if they aren't listened for.  If the user handled this error
-    // then we should try to reconnect.
 };
 
-TSDBClient.prototype.on_connect = function () {
-    if (exports.debug_mode) {
-        console.log("Query = http://" + this.host + ":" + this.port + "/" + this.version + "/" + this.query);
-    }
-    this.emit("query");
- };
+util.inherits(TSDBClient, events.EventEmitter);
+exports.TSDBClient = TSDBClient;
+
 
 // Makes things easier to see in debug
-function Query(query, params, callback) {
-    this.query = query;
+function Query(method, path, params, callback) {
+    this.method = method;
     this.params = params;
     this.callback = callback;
 };
 
-/* Maybe private 
-TSDBClient.prototype.send_query = function (query, params, callback) {
+// Private method that will make the API requests and assemble parameters
+var send_query = function (method, path, params, callback) {
 
-    var params, query_obj, i, stream = this.stream, query_str = "", lcaseCommand;
-
-    if (typeof query !== "string") {
-        throw new Error("First argument to send_query must be the command name string, not " + typeof query);
+    if ( method.toString() !== "GET" && method.toString() !== "POST" && method.toString() !== "PUT" ) {
+        throw new Error("First argument to send_query must be the request type GET, POST or PUT, not " + method);
     }
 
-    query_obj = new Query(query, params, callback);
+    if (typeof path !== "string") {
+        throw new Error("Second argument to send_query must be the command object, not " + typeof path);
+    }
 
-    if(query == "GET") {};
-    if(query == "POST") {};
-    if(query == "PUT") {};
+    if (typeof params !== "object") {
+        throw new Error("Third argument to send_query must be the command object, not " + typeof params);
+    }
+
+    query_obj = new Query(method, path, params, callback);
+
+    var url = "http://" + host + ":" + port + "/" + version + path;
+
+    if(method == "GET") { 
+
+        url = url + "/" + params.tid + "?token=" + params.timeseries.token;
+        console.log(url);
+        
+        request(url, function(err, httpResponse, body) {
+            if(err) {
+                callback(err, url);
+            }
+            callback(null,body);
+        });
+    };
+
+    if(method == "POST") {
+        
+        request.post({ url: url, body: params, json: true }, function(err, httpResponse, body) {
+            if(err) {
+                callback(err, "Failed to POST data");
+            }
+            callback(null,body);
+        });
+
+    };
+    if(method == "PUT") { 
+
+        url = url + "/" + params.tid + "?token=" + params.timeseries.token;
+        console.log(url);
+        console.log(params.data);
+        
+        request.put({ url: url, body: params.data, json: true }, function(err, httpResponse, body) {
+            if(err) {
+                callback(err, "Failed to PUT data");
+            }
+            callback(null,body);
+        });
+        
+    };
 
 };
-*/
 
+// Create
 exports.create = function (options, callback) {
     console.log(options);
-    callback(null,"Hello");
+    send_query("POST", "/ts", options, function(err, newts) {
+    
+        callback(err, newts);
+        
+    });
 };
 
-
-exports.TSDBClient = TSDBClient;
-
 // Read
+exports.read = function (options, callback) {
+    console.log(options);
+    send_query("GET", "/ts/tid", options, function(err, ts) {
+    
+        callback(err, ts);
+        
+    });
+};
 
-// Write
-
+// Update
+exports.update = function (options, data, callback) {
+    options.data = data;
+    console.log(JSON.stringify(options));
+    send_query("PUT", "/ts/tid", options, function(err, newts) {
+    
+        callback(err, newts);
+        
+    });
+};
